c40639ce4ce0b79a8035acb3709026435a8668bc
Linus Lüssing <linus.luessing@c0d3.blue>
2016-06-15 02:14:04 +0200

    Enable verbose logging by default

--- estimote-sticker/start.sh
+++ estimote-sticker/start.sh
@@ -3,6 +3,6 @@
 cd /home/nbsp/dev/estimote/node-bleacon/estimote-sticker
 
 while true; do
-	env DEBUG=box NOBLE_REPORT_ALL_HCI_EVENTS=1 node ./exil-sticker.js
+	env DEBUG=box,box-verbose NOBLE_REPORT_ALL_HCI_EVENTS=1 node ./exil-sticker.js
 	sleep 5
 done
